44cf039ba883a6386684bf87cb1d651b6e1299e9
Arijit2175 <arijitkarmakar2175@gmail.com>
2026-02-19 20:36:32 +0530

    pose and hand model tracking

--- clone.py
+++ clone.py
@@ -5,3 +5,7 @@ import numpy as np
 mp_pose = mp.solutions.pose
 mp_hands = mp.solutions.hands
 
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
+hands = mp_hands.Hands(max_num_hands=2)
+
